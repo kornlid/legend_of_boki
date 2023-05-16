3eb39dbe78cd1013d01e17f99a23392122ca55ab
soyeon bak <bluewaters1104@gamil.com>
2023-05-16 21:22:30 +0900

    보스 등장 및 랜덤 위치하는 부분 추가

--- 0516/maingame.py
+++ 0516/maingame.py
@@ -43,16 +43,16 @@ class WindowClass(QMainWindow, form_class):
         # 캐릭터의 위치에 따라 포탈 위치 변경
         random_spot = random.randrange(1, 4)
         if random_spot == 1:
-            self.label.move(790, 0)
+            self.label.move(790, 0)  # 캐릭터가 상단에 위치할 때
             self.portal_sample.move(random.randint(0, 1580), random.randint(390, 780))
         elif random_spot == 2:
-            self.label.move(0, 390)
+            self.label.move(0, 390)  # 캐릭터가 왼쪽 중앙에 위치할 때
             self.portal_sample.move(random.randint(790, 1580), random.randint(0, 780))
         elif random_spot == 3:
-            self.label.move(790, 780)
+            self.label.move(790, 780)  # 캐릭터가 하단에 위치할 때
             self.portal_sample.move(random.randint(0, 1580), random.randint(0, 390))
         elif random_spot == 4:
-            self.label.move(1580, 390)
+            self.label.move(1580, 390)  # 캐릭터가 오른쪽 중앙에 위치할 때ㅑ
             self.portal_sample.move(random.randint(0, 790), random.randint(0, 780))
 
         # 캐릭터 이미지 가져오기
@@ -62,8 +62,11 @@ class WindowClass(QMainWindow, form_class):
         # 이 부분에서 랜덤값으로 던전 맵 임시생성하는 부분 추가하기
         # 1, 2, 3 중 하나의 값이 나오면 스택드 위젯 값 가져오기
 
-        self.label_list = self.Page_Dungeon_Field.findChildren(QLabel)  # {던전 필드}에 있는 라벨 정보 가져와서 리스트에 저장
+        self.label_list = self.Page_Dungeon_Field.findChildren(QLabel)  # {던전 필드1}에 있는 라벨 정보 가져와서 리스트에 저장
         self.label_list_battlefield_2 = self.Page_Dungeon_Field_1.findChildren(QLabel)  # {던전 필드2}에 있는 라벨 정보 가져와서 리스트에 저장
+
+        for i in self.label_list:
+            print(i.objectName())
 
     def keyPressEvent(self, event):
         """키값 입력받아 라벨 움직이는 함수"""
@@ -88,19 +91,12 @@ class WindowClass(QMainWindow, form_class):
                 return
 
             # 현재위치 왼쪽 상단에 출력
-            self.TopUI_Coordinate_Label.setText(f"x좌표: {self.label.pos().x()}, y좌표:{self.label.pos().y()}")
+            self.TopUI_Coordinate_Label.setText(
+                f"x좌표: {self.label.pos().x()}, y좌표:{self.label.pos().y()}")  # 함수에서 구현되는지 확인하기
 
             # 일반필드에서 포탈 만났을 때
             if self.label.geometry().intersects(self.portal_sample.geometry()):  # 포탈 만나면
-                # last_position = self.label.geometry()  # 마지막 좌표 기억
-                # self.label_5.setGeometry(last_position) # 마지막 좌표로 이동한 후 다음 필드에서 동일한 곳에 떨어지게 함 -> 던전 크기 수정으로 취소
-                self.portal_sample.hide()  # 던전 필드로 이동할 때 포탈 숨겨주기
-                random_dungeon_num = random.randint(1,2) # 던전 랜덤으로 가는 값 추가(4까지 추가 예정)
-                if random_dungeon_num == 1:
-                    self.StackWidget_Field.setCurrentIndex(1)  # 1번 던전 필드로 이동
-                    self.Show_Dungeon_Entrance()  # 던전 입구 만들기
-                elif random_dungeon_num == 2:
-                    self.StackWidget_Field.setCurrentIndex(2) #2번 던전 필드로 이동
+                self.move_to_deongun()  # 랜덤으로 던전으로 이동
 
         # 던전입구1일 때
         elif current_index == 1:
@@ -132,17 +128,20 @@ class WindowClass(QMainWindow, form_class):
                 reply.exec_()
 
             # 라벨(벽과)겹치면 이전 위치로 이동하도록 함
-            for label in self.label_list:  # 라벨 정보 리스트 가져옴
+            for label in self.label_list:  # 라벨 정보 리d스트 가져옴
                 if label != self.label_5 and label != self.label_2 and new_position.intersects(label.geometry()):  # 만약 겹치면 break
                     print(f'{label.objectName()}와 겹침')  # 확인용
                     self.label_5.setGeometry(previous_position)
                     break
-            if not ((558 <= new_position.x() <= 1054) and (168 < new_position.y() < 662)): #캐릭터가 벗어나지 않게 하는 부분
+
+            # 던전 벽 캐릭터가 벗어나지 못하게
+            if not ((558 <= new_position.x() <= 1054) and (168 < new_position.y() < 662)):  # 미궁 x값, 미궁 y값 설정
                 self.label_5.setGeometry(previous_position)
 
-        #던전필드 2일때
+
+        # 던전필드 2일때
         elif current_index == 2:
-            previous_position_ = self.label_63.geometry()  # 움직이는 {라벨} 현재 위치 정보 가져옴 <= 이전위치
+            previous_position_2 = self.label_63.geometry()  # 움직이는 {라벨} 현재 위치 정보 가져옴 <= 이전위치
             if event.key() == Qt.Key_A:  # A 눌렀을 때
                 self.label_63.setPixmap(self.character_left_img)
                 new_position = self.label_63.geometry().translated(-20, 0)  # 새 포지션 값 저장
@@ -167,10 +166,40 @@ class WindowClass(QMainWindow, form_class):
             for label in self.label_list_battlefield_2:  # 라벨 정보 리스트 가져옴
                 if label != self.label_63 and label != self.label_26 and new_position.intersects(label.geometry()):  # 캐릭터 라벨, 배경 라벨은 제외.
                     print(f'{label.objectName()}와 겹침')  # 확인용
-                    self.label_63.setGeometry(previous_position_) # 겹치면 이전 위치로 이동
-                    break # for문 break
+                    self.label_63.setGeometry(previous_position_2)  # 겹치면 이전 위치로 이동
+                    break  # for문 break
             # if not ((558 <= new_position.x() <= 1054) and (168 < new_position.y() < 662)):
             #     self.label_5.setGeometry(previous_position)
+
+    # 함수로 키 입력 단순화 해보려고 했으나 실패. 시도했지만 코드를 줄이는데 큰 영향을 안 줌
+    # def move_label(self, label, direction, distance):
+    #     """라벨 움직이는 부분 함수로 연결"""
+    #     if direction == 'left':
+    #         label.setPixmap(self.character_left_img)
+    #         label.move(label.x() - distance, label.y())
+    #     elif direction == 'right':
+    #         label.setPixmap(self.character_right_img)
+    #         label.move(label.x() + distance, label.y())
+    #     elif direction == 'up':
+    #         label.setPixmap(self.character_left_img)
+    #         label.move(label.x(), label.y() - distance)
+    #     elif direction == 'down':
+    #         label.setPixmap(self.character_left_img)
+    #         label.move(label.x(), label.y() + distance)
+    # 
+    #     self.TopUI_Coordinate_Label.setText(f"x좌표: {label.pos().x()}, y좌표:{label.pos().y()}") #왼쪽상단에 현재위치 표시
+
+    def move_to_deongun(self):
+        """던전으로 이동하는 부분"""
+        # last_position = self.label.geometry()  # 마지막 좌표 기억
+        # self.label_5.setGeometry(last_position) # 마지막 좌표로 이동한 후 다음 필드에서 동일한 곳에 떨어지게 함 -> 던전 크기 수정으로 취소
+        self.portal_sample.hide()  # 던전 필드로 이동할 때 포탈 숨겨주기
+        random_dungeon_num = 1  # random.randint(1, 2)  # 던전 랜덤으로 가는 값 추가(4까지 추가 예정)
+        if random_dungeon_num == 1:
+            self.StackWidget_Field.setCurrentIndex(1)  # 1번 던전 필드로 이동
+            self.Show_Dungeon_Entrance()  # 던전 입구 만들기
+        elif random_dungeon_num == 2:
+            self.StackWidget_Field.setCurrentIndex(2)  # 2번 던전 필드로 이동
 
     def Show_Dungeon_Entrance(self):
         """던전 입구 랜덤으로 만들어주는 함수"""
@@ -180,9 +209,22 @@ class WindowClass(QMainWindow, form_class):
         self.entrance.setText("미궁")
         self.entrance.setFixedSize(30, 30)  # 임시 라벨지정
         self.entrance.setStyleSheet('background-color: blue')
-        self.entrance.move(random.randint(558, 1054), random.randint(168, 662))
+        self.entrance.move(random.randint(558, 1054), random.randint(168, 662)) #던전 15*15 사이즈
+
+        #몬스터 위치 임시로 만들어주기
+        self.monster = QLabel(self)
+        self.monster.setText("몬스터")
+        self.monster.setFixedSize(30, 30)  # 임시 라벨지정
+        self.monster.setStyleSheet('background-color: red')
+        self.monster.move(random.randint(558, 1054), random.randint(168, 662))
+
+
+
         self.check_collision(self.entrance)  # 미궁 만들어지는 곳 중복체크 하는 함수로 이동하기
+        self.check_collision(self.monster)  # 미궁 만들어지는 곳 중복체크 하는 함수로 이동하기
+
         self.entrance.show()
+        self.monster.show()
 
         # 검은 라벨 만들어서 위에 덮기(유저가 플레이할 때 던전이 안보이게)
         # black_label = QLabel(self)
@@ -193,8 +235,12 @@ class WindowClass(QMainWindow, form_class):
 
     def check_collision(self, label):
         """새로 미궁 만들어지는 곳 중복체크 하는 함수"""
-        self.label_list.remove(self.label_2)  # 배경 라벨 제거 # 현재는 주석처리 해놓음
-        self.label_list.remove(self.label_5)  # 캐릭터 라벨 제거 # 현재는 주석처리 해놓음
+        if self.label_2 in self.label_list:
+            self.label_list.remove(self.label_2) # 캐릭터 지우기
+        if self.label_5 in self.label_list:
+            self.label_list.remove(self.label_5) # 배경 지우기
+
+
         for other_label in self.label_list:  # 라벨이 겹치면 값 재설정
             if label.geometry().intersects(other_label.geometry()):
                 label.move(random.randint(558, 1054), random.randint(168, 662))
