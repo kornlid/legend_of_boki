b2b34a83089634da2a36c6f4bf882a0ee433f0be
guaba98 <bluewaters1104@gmail.com>
2023-05-20 14:21:25 +0900

    캐릭터 배경깜빡임 부분 테스트

--- ghost_moving_test/ghost_moving.py
+++ ghost_moving_test/ghost_moving.py
@@ -10,11 +10,11 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.move(200, 200)
-        self.setFixedSize(700, 800)
+        self.setGeometry(200, 200, 300, 300)
         self.setStyleSheet("background-color:black")
 
         # Create a label
-        # 이미지 불러오기
+        # 유령 이미지 불러오기
         self.ghost_img_top = QPixmap('./ghost_img/ghost_top.png')  # 귀신 이미지 상
         self.ghost_img_right = QPixmap('./ghost_img/ghost_right.png')  # 우
         self.ghost_img_left = QPixmap('./ghost_img/ghost_left.png')  # 좌
@@ -24,16 +24,25 @@ class MainWindow(QMainWindow):
         self.ghost_img_left_bottom = QPixmap('./ghost_img/ghost_left_bottom.png')  # 좌하
         self.ghost_img_right_bottom = QPixmap('./ghost_img/ghost_right_bottom.png')  # 좌하
 
-        self.label = QLabel(self)
-        self.label.setPixmap(self.ghost_img_right.scaled(QSize(200, 200), aspectRatioMode=Qt.IgnoreAspectRatio))
-        self.label.setGeometry(300, 300, 300, 300)
+        # 유령 크기 고정해주기
+        self.ghost_fixed_size = 100
 
         self.character = QLabel(self)
-        self.character.setStyleSheet('background-color: yellow')
-        self.character.setFixedSize(30, 50)
+        self.character.setStyleSheet('border: 4px solid yellow')
+        self.character.setFixedSize(300, 300)
+        self.character.move(0, 0)
         self.character.show()
 
+        # 유령 담길 라벨 만들어주기
+        self.ghost_label = QLabel(self)
+        self.ghost_label.setPixmap(self.ghost_img_right.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        self.ghost_label.move(10, 10)
+        self.ghost_label.setFixedSize(100, 100)
+
+
+
         self.random_num = None
+
         # 방향 타이머
         self.position = QTimer()
         self.position.setInterval(3000)
@@ -46,64 +55,121 @@ class MainWindow(QMainWindow):
         self.timer.start(40)  # Move the label every 100 milliseconds
 
     def direction(self):
+        """유령 방향 랜덤값 반환"""
         random_num = random.randint(1, 8)
         self.random_num = random_num
 
     def move_label(self):
+        """유령 움직임 조정 함수"""
+        # # 현재 라벨 포지션 받기
+        # current_pos = self.ghost_label.pos()
+        #
+        # # 창의 최대값 - 현재 라벨 길이값(약간 수정 필요)
+        # max_x = self.width() - self.ghost_label.width()
+        # max_y = self.height() - self.ghost_label.height()
+        #
+        #
+        #
+        # # 새 위치 계산
+        # if self.random_num == 1:  # 우하
+        #     self.ghost_label.setPixmap(
+        #         self.ghost_img_right_bottom.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = min(current_pos.x() + 1, max_x)
+        #     new_y = min(current_pos.y() + 1, max_y)
+        # elif self.random_num == 2:  # 우상
+        #     self.ghost_label.setPixmap(
+        #         self.ghost_img_right_top.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = min(current_pos.x() + 1, max_x)
+        #     new_y = max(current_pos.y() - 1, 0)
+        # elif self.random_num == 3:  # 좌상
+        #     self.ghost_label.setPixmap(
+        #         self.ghost_img_left_top.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = max(current_pos.x() - 1, 0)
+        #     new_y = max(current_pos.y() - 1, 0)
+        # elif self.random_num == 4:  # 좌하
+        #     self.ghost_label.setPixmap(
+        #         self.ghost_img_left_bottom.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = max(current_pos.x() - 1, 0)
+        #     new_y = min(current_pos.y() + 1, max_y)
+        # elif self.random_num == 5:  # 왼쪽
+        #     self.ghost_label.setPixmap(self.ghost_img_left.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = max(current_pos.x() - 1, 0)
+        #     new_y = current_pos.y()
+        # elif self.random_num == 6:  # 오른쪽
+        #     self.ghost_label.setPixmap(self.ghost_img_right.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = min(current_pos.x() + 1, max_x)
+        #     new_y = current_pos.y()
+        # elif self.random_num == 7:  # 상
+        #     self.ghost_label.setPixmap(self.ghost_img_top.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = current_pos.x()
+        #     new_y = max(current_pos.y() - 1, 0)
+        # else:  # 하
+        #     self.ghost_label.setPixmap(
+        #         self.ghost_img_bottom.scaled(QSize(self.ghost_fixed_size, self.ghost_fixed_size), aspectRatioMode=Qt.IgnoreAspectRatio))
+        #     new_x = current_pos.x()
+        #     new_y = min(current_pos.y() + 1, max_y)
+        #
+        # # Set the new position of the label
+        # self.ghost_label.move(new_x, new_y)
+
+
         # Get the current position of the label
-        current_pos = self.label.pos()
-        if not self.current_pos.x()
+        current_pos = self.ghost_label.pos()
+
+        # Define the range of x and y positions
+        x_start = 0  # 시작 x 값
+        x_end = 300  # self.width() - self.label.width()  # 끝 x 값
+        y_start = 0  # 시작 y 값
+        y_end = 300  # self.height() - self.label.height()  # 끝 y 값
 
         # Calculate the new position
         if self.random_num == 1:  # 우하
-            self.label.setPixmap(
+            self.ghost_label.setPixmap(
                 self.ghost_img_right_bottom.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() + 1  # 오른쪽으로 1 움직이게 하기
-            new_y = current_pos.y() + 1  # 아래로 1 움직이게 하기
+            new_x = min(current_pos.x() + 1, x_end)
+            new_y = min(current_pos.y() + 1, y_end)
         elif self.random_num == 2:  # 우상
-            self.label.setPixmap(
+            self.ghost_label.setPixmap(
                 self.ghost_img_right_top.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() + 1  # 오른쪽으로 1 움직이게 하기
-            new_y = current_pos.y() - 1  # 위로 1 움직이게 하기
+            new_x = min(current_pos.x() + 1, x_end)
+            new_y = max(current_pos.y() - 1, y_start)
         elif self.random_num == 3:  # 좌상
-            self.label.setPixmap(
+            self.ghost_label.setPixmap(
                 self.ghost_img_left_top.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() - 1  # 오른쪽으로 1 움직이게 하기
-            new_y = current_pos.y() - 1  # 아래로 1 움직이게 하기
-        elif self.random_num == 4:  # 좌상
-            self.label.setPixmap(
+            new_x = max(current_pos.x() - 1, x_start)
+            new_y = max(current_pos.y() - 1, y_start)
+        elif self.random_num == 4:  # 좌하
+            self.ghost_label.setPixmap(
                 self.ghost_img_left_bottom.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() - 1  # 오른쪽으로 1 움직이게 하기
-            new_y = current_pos.y() + 1  # 아래로 1 움직이게 하기
+            new_x = max(current_pos.x() - 1, x_start)
+            new_y = min(current_pos.y() + 1, y_end)
         elif self.random_num == 5:  # 왼쪽
-            self.label.setPixmap(
-                self.ghost_img_left.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() - 1  # 왼쪽으로만 움직이게 하기
+            self.ghost_label.setPixmap(self.ghost_img_left.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
+            new_x = max(current_pos.x() - 1, x_start)
             new_y = current_pos.y()
-        elif self.random_num == 6:
-            self.label.setPixmap(
-                self.ghost_img_right.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
-            new_x = current_pos.x() + 1  # 오른쪽으로만 움직이게 하기
+        elif self.random_num == 6:  # 오른쪽
+            self.ghost_label.setPixmap(self.ghost_img_right.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
+            new_x = min(current_pos.x() + 1, x_end)
             new_y = current_pos.y()
-        elif self.random_num == 7:
-            self.label.setPixmap(
-                self.ghost_img_top.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
+        elif self.random_num == 7:  # 상
+            self.ghost_label.setPixmap(self.ghost_img_top.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
             new_x = current_pos.x()
-            new_y = current_pos.y() - 1
-        else:
-            self.label.setPixmap(
+            new_y = max(current_pos.y() - 1, y_start)
+        else:  # 하
+            self.ghost_label.setPixmap(
                 self.ghost_img_bottom.scaled(QSize(100, 100), aspectRatioMode=Qt.IgnoreAspectRatio))
             new_x = current_pos.x()
-            new_y = current_pos.y() + 1
+            new_y = min(current_pos.y() + 1, y_end)
+
         # Set the new position of the label
-        self.label.move(new_x, new_y)
+        self.ghost_label.move(new_x, new_y)
 
     def keyPressEvent(self, event):
         x_positon = self.character.pos().x()
         y_position = self.character.pos().y()
 
-        label_x_positon = self.label.pos().x()
-        label_y_positoin = self.label.pos().y()
+        label_x_positon = self.ghost_label.pos().x()
+        label_y_positoin = self.ghost_label.pos().y()
 
         if event.key() == Qt.Key_Right:
             self.character.move(x_positon + 20, y_position)
